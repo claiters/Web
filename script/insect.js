be02858a6c2dbd0b0a56cf60737b5c76e56e6e9b
claiters <jacobandfriends45@gmail.com>
2026-04-07 07:23:58 +0000

    Tue, Apr 7, 2026, 12:23 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,14 +1,14 @@
 const screens = document.querySelectorAll('.screen')
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-const game_ container = document.getElementById('game-container')
+const game_container = document.getElementById('game-container')
 const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
-const scoreEl = documentgetElementById('score')
+const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 let seconds = 0
 let score = 0
 let selected_insect = {}
 
-start+btn.addEventListener('click', () => {
+start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
 })
